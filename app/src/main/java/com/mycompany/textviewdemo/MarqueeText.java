067bb7eb64b71b1894a2f11a783ad8244054fc8e
dadangjiahjz <412440405@qq.com>
2015-04-27 23:30:44 +0800

    0427_v1.0

---
+++ app/src/main/java/com/mycompany/textviewdemo/MarqueeText.java
@@ -0,0 +1,31 @@
+package com.mycompany.textviewdemo;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by H.J.Z on 2015/4/27.
+ */
+public class MarqueeText extends TextView {
+    public MarqueeText(Context context) {
+        super(context);
+    }
+
+    public MarqueeText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MarqueeText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+
+
+
+    @Override
+    public boolean isFocused() {
+        return true;
+    }
+}
